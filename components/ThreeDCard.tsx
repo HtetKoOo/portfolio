9fedb32f791a6d44548c10d791352bd37cf1f567
Htet Ko Oo <htetkooo2532@gmail.com>
2025-12-01 15:48:23 +0700

    fix: floating navbar for mobile view

--- components/ThreeDCard.tsx
+++ components/ThreeDCard.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export function ThreeDCard({project}: any) {
     return (
         <CardContainer className="inter-var">
-            <CardBody className="bg-transparent relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-3 border  ">
+            <CardBody className="bg-transparent relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/10 dark:border-white/20 border-black/10 w-full md:w-3/4 lg:w-auto h-auto rounded-xl p-5 border">
                 <CardItem
                     translateZ="50"
                     className="text-xl font-bold"
@@ -16,25 +16,32 @@ export function ThreeDCard({project}: any) {
                 <CardItem
                     as="p"
                     translateZ="60"
-                    className="text-sm max-w-sm mt-2"
+                    className="text-sm mt-3"
                 >
                     {project.des}
                 </CardItem>
-                <CardItem translateZ="100" className="w-full mt-4">
+                <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-sm mt-3"
+                >
+                    <span className="font-semibold">Tech Stack :</span> {project.tech_stack}
+                </CardItem>
+                <CardItem translateZ="100" className="w-full mt-5">
                     <Image
                         src={project.img}
                         height={1000}
                         width={1000}
-                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                        className="h-auto w-full object-cover rounded-xl group-hover/card:shadow-xl"
                         alt="thumbnail"
                     />
                 </CardItem>
-                <div className="flex justify-between items-center mt-4">
+                <div className="flex justify-between items-center mt-5">
                     <CardItem
                         translateZ={20}
                         as="a"
                         href={project.github_link}
-                        target="__blank"
+                        target="_blank"
                         className="px-4 py-2 rounded-xl text-xs font-normal"
                     >
                         Source Code →
@@ -43,7 +50,7 @@ export function ThreeDCard({project}: any) {
                         translateZ={20}
                         as="a"
                         href={project.live_link}
-                        target="__blank"
+                        target="_blank"
                         className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                     >
                         Live Demo →
