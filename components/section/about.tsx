1359e5855b0bb7ff59bc3181513a5d5463c5da80
Htet Ko Oo <htetkooo2532@gmail.com>
2025-12-02 10:45:03 +0700

    adding motion for each part

--- components/section/about.tsx
+++ components/section/about.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { motion } from "framer-motion";
 import { Download } from "lucide-react";
 import Image from "next/image";
 import { Badge } from "../ui/badge";
@@ -18,7 +19,12 @@ export default function About() {
         >
             <div className="max-w-5xl w-full flex flex-col lg:flex-row gap-6 items-center">
                 {/* Left: Image */}
-                <div className="flex w-full lg:w-1/3 justify-center">
+                <motion.div
+                    initial={{ opacity: 0, x: -100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1, delay: 0.5 }}
+                    viewport={{ once: true }}
+                    className="flex w-full lg:w-1/3 justify-center">
                     <Image
                         src="/profile-photo.png"
                         alt="Profile Photo"
@@ -26,38 +32,63 @@ export default function About() {
                         height={300}
                         className="rounded-2xl shadow-lg object-cover w-60 h-60 md:w-72 md:h-72 lg:h-84"
                     />
-                </div>
+                </motion.div>
 
                 {/* Right: Text Content */}
                 <div className="flex flex-col w-full md:w-2/3 gap-4">
                     <div className="text-center lg:text-left"><Badge variant="secondary">About Me</Badge></div>
-                    
+
                     <TextGenerateEffect
                         words="React, Next.js & Laravel Enthusiast"
                         className="text-center lg:text-left text-3xl md:text-4xl font-semibold"
                     />
-                    <p className="text-default-600 leading-relaxed text-base md:text-lg">
+                    <motion.p
+                        initial={{ opacity: 0, x: 100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 1, delay: 0.5 }}
+                        viewport={{ once: true }}
+                        className="text-default-600 leading-relaxed text-base md:text-lg"
+                    >
                         I’m a passionate <span className="text-primary font-medium">Full Stack Developer</span> specializing in
                         building modern, responsive, and scalable web applications using
                         <span className="text-primary font-medium"> React, Next.js, and Laravel</span>.
-                    </p>
-                    <p className="text-default-600 leading-relaxed text-base md:text-lg">
+                    </motion.p>
+
+                    <motion.p
+                        initial={{ opacity: 0, x: 100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 1, delay: 1 }}
+                        viewport={{ once: true }}
+                        className="text-default-600 leading-relaxed text-base md:text-lg"
+                    >
                         I love transforming ideas into elegant, functional, and user-friendly products.
                         My focus is on writing clean code, crafting engaging UI/UX, and continuously
-                        learning new technologies to improve my craft.My professional contributions include successfully working on projects such as a Utility Management System, a comprehensive Project Management System, and portfolio websites.
-                    </p>
-                    <p className="text-default-600 leading-relaxed text-base md:text-lg">
-                        I possess a broad and adaptive technical skillet beyond my core stack, having explored and gained exposure to Python/Django for backend development, as well as cross-platform mobile development with React Native and Flutter. I am dedicated to continuous improvement and am presently focused on acquiring proficiency in Vue.js to further diversify my frontend capabilities.
-                    </p>
-                    <div className="flex mt-4 items-center md:justify-between lg:justify-start gap-3">
+                        learning new technologies to improve my craft. My professional contributions include working on projects like a Utility Management System, a comprehensive Project Management System, and portfolio websites.
+                    </motion.p>
+
+                    <motion.p
+                        initial={{ opacity: 0, x: 60 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 1, delay: 1.5 }}
+                        viewport={{ once: true }}
+                        className="text-default-600 leading-relaxed text-base md:text-lg"
+                    >
+                        I possess a broad and adaptive technical skillset beyond my core stack, having explored Python/Django for backend development, and React Native & Flutter for mobile. I&apos;m currently expanding into Vue.js to further enhance my frontend capabilities.
+                    </motion.p>
+
+                    <motion.div
+                        initial={{ opacity: 0, y: 60 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1, delay: 1.5 }}
+                        viewport={{ once: true }}
+                        className="flex mt-4 items-center md:justify-between lg:justify-start gap-3">
                         {/* Action: download */}
                         <MagicButton
                             title="Download Resume"
                             icon={<Download size={20} />}
                             position="left"
                         />
-
-                        {/* Navigation: external link */}
+                        {/* Navigation: github link */}
                         <Link href={links.github} target="_blank" rel="noopener noreferrer">
                             <MagicButton
                                 title="GitHub"
@@ -66,11 +97,10 @@ export default function About() {
                                 otherClasses="bg-gray-500 dark:bg-gray-700 text-white"
                             />
                         </Link>
-                    </div>
-
+                    </motion.div>
                 </div>
             </div>
-        </section>
+        </section >
 
     );
 }
